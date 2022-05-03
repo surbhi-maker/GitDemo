53b6f9c743ae065f9a5d2af89040a69aac622e96
surbhi-maker <surbhimamar99@gmail.com>
2022-05-03 17:31:55 +0530

    changes done in new file

--- src/main/java/testing.java
+++ src/main/java/testing.java
@@ -2,5 +2,7 @@ public class testing {
 
 	public static void main(String[] args) {
 		System.out.println("Any String");
+		System.out.println("Any String 1");
+		System.out.println("Any String 2");
 	}
 }
